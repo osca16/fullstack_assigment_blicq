11841e742c6d892f32bec76712ec8b4a9ff5e10f
Osanda Lakruwan <osandalakruwan516@gmail.com>
2026-04-19 13:10:27 +0530

    prisma schema configuration and prisma package updates

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,11 @@
-import GoogleProvider from 'next-auth/providers/google';
-import { signIn } from 'next-auth/react';
+import type { NextAuthConfig } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/src/lib/prisma";
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -9,21 +13,32 @@ export const authOptions = {
         }),
     ],
 
-    session: {
-        strategy:"jwt", 
-    },
-
-    callbacks:{
-        async signIn({user}) {
-            if(!user.email) return false;
+    callbacks: {
+        async signIn({ user }) {
+            if (!user.email) {
+                return false;
+            }
 
             const existingUser = await prisma.user.findUnique({
-                where:{email:user.email},
+                where: { email: user.email },
             });
 
-            if(existingUser){
+            if (existingUser) {
                 return true;
             }
-        }
+
+            await prisma.user.create({
+                data: {
+                    email: user.email,
+                    name: user.name,
+                    role: "USER",
+                },
+            });
+
+            return true;
+        },
     },
-}
+    session: {
+        strategy: "jwt",
+    },
+} satisfies NextAuthConfig;
