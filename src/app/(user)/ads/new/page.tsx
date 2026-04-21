149e8448eb308921bff347395df43cb98c1da049
Osanda Lakruwan <osandalakruwan516@gmail.com>
2026-04-21 20:02:41 +0530

    ad form page created

--- src/app/(user)/ads/new/page.tsx
+++ src/app/(user)/ads/new/page.tsx
@@ -1,3 +1,11 @@
+
+import AdForm from "@/src/components/user/AdForm";
+
+
 export default function NewAdPage() {
-	return <main>Create new ad page</main>;
+	return (
+		<main className="mx-auto w-full max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+			<AdForm/>
+		</main>
+	);
 }
