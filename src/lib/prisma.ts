1173859acb878a5f16f52b11a1d33407823922bd
Osanda Lakruwan <osandalakruwan516@gmail.com>
2026-04-21 19:50:29 +0530

    fixed figma query showing in the consol

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
 	globalForPrisma.prisma ||
 	new PrismaClient({
-		log: ["query"],
+		log: process.env.PRISMA_QUERY_LOG === "true" ? ["query"] : ["error", "warn"],
 	});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
